c1e12f441ef66bdfe88eb522db7be4eef46c76cc
student <naamanboutigh@hotmail.com>
2018-01-23 16:03:06 -0800

    Final release

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,145 @@
 from styx_msgs.msg import TrafficLight
+import rospy
+
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+import cv2
+
+from PIL import Image
+import visualization_utils as vis_util
+
+print(tf.__version__)
+
+
+def load_image_into_numpy_array(image):
+    return np.asarray(image, dtype="uint8")
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+        # Path to frozen detection graph. This is the actual model that is used for the object detection.
+        self.path_to_ckpt = MODEL_NAME + '/frozen_inference_graph.pb'
+
+        self.index_traffic = 10
+        self.num_classes = 90
+        self.detection_graph = tf.Graph()
+        self.traffic_light_color = 4
+
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        with self.detection_graph.as_default():
+            with tf.Session(graph=self.detection_graph) as sess:
+                self.sess = sess
+                # Definite input and output Tensors for detection_graph
+                self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+                self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+                self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        image_np = load_image_into_numpy_array(image)
+        # Expand dimensions since the tensorflow model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+
+        # In the model data, traffic lights have the index number 10
+        category_index = {10: {'id': 10, 'name': 'traffic light'}}
+
+        height, width, _ = image_np.shape
+        classes = np.squeeze(classes).astype(np.int32)
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        confidence = 0
+        box_conf = []
+        classes_10 = []
+        box_area = 0
+        for i in range(len(classes)):
+            if classes[i] in category_index.keys():
+                if scores[i] > confidence:
+                    confidence = scores[i]
+                    box_conf = boxes[i]
+                    classes_10.append(classes[i])
+                    if len(box_conf) != 0:
+                        ymin, xmin, ymax, xmax = box_conf
+                        (left, right, top, bottom) = (xmin * width, xmax * width,
+                                                      ymin * height, ymax * height)
+                        box_area = (int(bottom) - int(top)) * (int(right) - int(left))
+                    else:
+                        box_area = 0
+
+        if not ((box_area < 1000) or (confidence < 0.5)):
+
+            rospy.loginfo("TL: box are  %s confidence is %s", box_area, confidence)
+
+            img_light = image_np[int(top):int(bottom), int(left):int(right)]
+
+            if 1:
+                traffic_light = cv2.resize(img_light, (64, 128))
+
+                tl_hsv = cv2.cvtColor(traffic_light, cv2.COLOR_BGR2HSV)
+
+                red_low = np.array([0, 50, 50])
+                red_up = np.array([10, 255, 255])
+                red1 = cv2.inRange(tl_hsv, red_low, red_up)
+
+                red_low = np.array([170, 50, 50])
+                red_up = np.array([180, 255, 255])
+                red2 = cv2.inRange(tl_hsv, red_low, red_up)
+
+                yel_low = np.array([20, 100, 100])
+                yel_up = np.array([40, 255, 255])
+                yel = cv2.inRange(tl_hsv, yel_low, yel_up)
+
+                weighted_img = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
+                blur = cv2.GaussianBlur(weighted_img, (15, 15), 0)
+                blur_yel = cv2.GaussianBlur(yel, (15, 15), 0)
+
+                circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 0.5, 40, param1=70, param2=20, minRadius=2,
+                                           maxRadius=20)
+                circles_yel = cv2.HoughCircles(blur_yel, cv2.HOUGH_GRADIENT, 0.5, 40, param1=70, param2=20, minRadius=2,
+                                               maxRadius=20)
+
+                if circles is None:
+                    if circles_yel is None:
+                        rospy.loginfo("TL: traffic light Green")
+                        self.traffic_light_color = TrafficLight.GREEN
+                    else:
+                        rospy.loginfo("TL: traffic light Yellow")
+                        self.traffic_light_color = TrafficLight.YELLOW
+                else:
+                    rospy.loginfo("TL: traffic light RED")
+                    self.traffic_light_color = TrafficLight.RED
+
+                category_color = {10: {'id': 10, 'name': self.traffic_light_color}}
+                box_show = []
+                box_show.append(box_conf)
+                box_to_color_map = vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    np.array(box_show),
+                    np.array(classes_10),
+                    scores,
+                    category_color,
+                    use_normalized_coordinates=True,
+                    line_thickness=16)
+        return (self.traffic_light_color, image_np)
--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -3,33 +3,31 @@
 import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, Twist
 import math
 
 from twist_controller import Controller
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
-
 You will subscribe to `/twist_cmd` message which provides the proposed linear and angular velocities.
 You can subscribe to any other message that you find important or refer to the document for list
 of messages subscribed to by the reference implementation of this node.
-
 One thing to keep in mind while building this node and the `twist_controller` class is the status
 of `dbw_enabled`. While in the simulator, its enabled all the time, in the real car, that will
 not be the case. This may cause your PID controller to accumulate error because the car could
 temporarily be driven by a human instead of your controller.
-
 We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
 wheel_base) etc should not be altered in these files.
-
 We have also provided some reference implementations for PID controller and other utility classes.
 You are free to use them or build your own.
-
 Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
 that we have created in the `__init__` function.
-
 '''
+
 
 class DBWNode(object):
     def __init__(self):
@@ -46,52 +44,102 @@ class DBWNode(object):
         max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
         max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
 
-        self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
-                                         SteeringCmd, queue_size=1)
-        self.throttle_pub = rospy.Publisher('/vehicle/throttle_cmd',
-                                            ThrottleCmd, queue_size=1)
-        self.brake_pub = rospy.Publisher('/vehicle/brake_cmd',
-                                         BrakeCmd, queue_size=1)
+        # Minimum speed at which we steer the vehicle
+        min_speed = 0.0
+
+        # Throttle/Brake PID parameters
+        k_p = 0.5
+        k_i = 0.0
+        k_d = 0.1
+
+        tau = 0.2
+        ts = 0.1
+
+        self.dbw_enabled = False  # subscribe from /vehicle/dbw_enabled
+        self.target_velocity = Twist()  # subscribe from /twist_cmd
+        self.current_velocity = Twist()  # subsribe from /current_velocity
+        self.previous_throttle = 0
+        self.previous_steering = 0
+        self.previous_brake = 0
+
+        self.steer_pub = rospy.Publisher('/vehicle/steering_cmd', SteeringCmd, queue_size=1)
+        self.throttle_pub = rospy.Publisher('/vehicle/throttle_cmd', ThrottleCmd, queue_size=1)
+        self.brake_pub = rospy.Publisher('/vehicle/brake_cmd', BrakeCmd, queue_size=1)
 
         # TODO: Create `TwistController` object
-        # self.controller = TwistController(<Arguments you wish to provide>)
+        yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        pid_controller = PID(k_p, k_i, k_d, decel_limit, accel_limit)
+        low_pass_filter = LowPassFilter(tau, ts)
+
+        self.controller = Controller(yaw_controller, pid_controller, low_pass_filter, vehicle_mass, fuel_capacity,
+                                     wheel_radius)
 
         # TODO: Subscribe to all the topics you need to
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_cb)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_cb)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
 
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(50)  # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+
+            throttle, brake, steering = self.controller.control(self.target_velocity.linear,
+                                                                self.target_velocity.angular,
+                                                                self.current_velocity.linear,
+                                                                self.dbw_enabled)  # ,
+            # <any other argument you need>)
+
+            if self.dbw_enabled:
+                self.publish(throttle, brake, steering)
+
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
-        tcmd.enable = True
+        tcmd.enable = (abs(throttle - self.previous_throttle) > 0.05)
+        if tcmd.enable:
+            self.previous_throttle = throttle
         tcmd.pedal_cmd_type = ThrottleCmd.CMD_PERCENT
         tcmd.pedal_cmd = throttle
         self.throttle_pub.publish(tcmd)
 
         scmd = SteeringCmd()
-        scmd.enable = True
+        scmd.enable = (abs(throttle - self.previous_steering) > 0.05)
+        if scmd.enable:
+            self.previous_steer = steer
         scmd.steering_wheel_angle_cmd = steer
         self.steer_pub.publish(scmd)
 
         bcmd = BrakeCmd()
-        bcmd.enable = True
+        bcmd.enable = (abs(brake - self.previous_brake) > 0.05)
+        if bcmd.enable:
+            self.previous_brake = brake
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
 
+    def twist_cmd_cb(self, msg):
+
+
+        self.target_velocity = msg.twist
+        pass
+
+    def current_velocity_cb(self, msg):
+
+        self.current_velocity = msg.twist
+        pass
+
+    def dbw_enabled_cb(self, msg):
+        self.dbw_enabled = msg.data
+        pass
+
 
 if __name__ == '__main__':
-    DBWNode()
+    try:
+        DBWNode()
+    except rospy.ROSInterruptException:
+        rospy.logerr('Error: Not start dbw_node!')
